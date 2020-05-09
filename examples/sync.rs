59763b0ecc92b08f9c1963a3a6a219b02e116a30
Richard Bradfield <bradfier@fstab.me>
2020-05-09 08:42:35 +0100

    Add synchronous example
    Shows how it's possible (although a little clunky without the buffer
    handling done for you) to use the decoder in synchronous code.

---
+++ examples/sync.rs
@@ -0,0 +1,37 @@
+use std::net::TcpStream;
+use std::io::Read;
+use bytes::{BufMut, BytesMut};
+use rmonitor::codec::RMonitorDecoder;
+use tokio_util::codec::Decoder;
+
+fn main() {
+    let mut stream = TcpStream::connect("127.0.0.1:4000")
+        .expect("Failed to open connection");
+
+    // .read() won't use the BytesMut directly in the appropriate manner,
+    // so we do one extra bit of buffering here
+    let mut read_buf = [0u8; 256];
+
+    // Create the decode buffer, and a decoder with a maximum line length
+    // of 2048.
+    let mut buffer = BytesMut::with_capacity(4096);
+    let mut decoder = RMonitorDecoder::new(2048);
+
+    loop {
+        let r = stream.read(&mut read_buf)
+            .expect("Failed to read from stream");
+
+        // Put only the read bytes into the decode buffer (not any trailing 0s)
+        if r > 0 {
+            buffer.put(&read_buf[..r]);
+        }
+
+        let maybe_record = decoder.decode(&mut buffer);
+
+        match maybe_record {
+            Ok(None) => { continue; }
+            Ok(Some(r)) => println!("{:?}", r),
+            Err(e) => println!("{:?}", e)
+        }
+    }
+}
