43be8949b17c0f94439f947b50c4f82ecdc9595e
Richard Bradfield <bradfier@fstab.me>
2020-05-09 13:38:08 +0100

    One-liner documentation prior to Crates.io publication

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
+//! A simple Tokio-compatible [`Decoder`] implementation of the [RMonitor Protocol]
+//! supported by a variety of sports timing software.
+//!
+//! [`Decoder`]: tokio_util::codec::Decoder
+//! [RMonitor Protocol]: https://github.com/bradfier/rmonitor/blob/master/docs/RMonitor%20Timing%20Protocol.pdf
+
 pub mod codec;
 pub mod protocol;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
